a36fb8bb551277b7c611bdabd055231cc2d8287a
JoaoG <165217153+JoaoGFurtadoSousa@users.noreply.github.com>
2026-02-23 23:17:28 -0300

    feat(Cars): Adicionando validacao de tipo para o formulario de novo_carro

--- cars/views.py
+++ cars/views.py
@@ -1,20 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import CarrosForm
 from .models import Carros
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+
 def novo_carro_form(request):
-    if request.method == 'GET':
-        return render(request, 'carros/novo_carro.html', {'novo_carro_form': CarrosForm})
-    else:
-        nome_do_carro = request.POST.get('nome_do_carro')
-        preco = request.POST.get('preco')
-        ano = request.POST.get('ano')
-        foto_carro = request.POST.get('foto_carro')
-        novo_carro = Carros.objects.create(nome_do_carro= nome_do_carro,
-                                           preco = preco,
-                                           ano = ano,
-                                           foto_carro = foto_carro)
-        novo_carro.save()
-        return HttpResponse({'mensagem': 'Carro salvo no banco'})
+    if request.session.get('user_session'):
+        if request.method == 'GET':
+            return render(request, 'carros/novo_carro.html', {'novo_carro_form': CarrosForm})
+        else:
+            form_carro = CarrosForm(request.POST)
+            print(request.POST)
+            if form_carro.is_valid():
+                nome_do_carro = form_carro.data['nome_do_carro']
+                preco = form_carro.data['preco']
+                ano = form_carro.data['ano']
+                foto_carro = form_carro.data['foto_carro']
+                novo_carro = Carros.objects.create(nome_do_carro= nome_do_carro,
+                                                preco = preco,
+                                                ano = ano,
+                                                foto_carro = foto_carro)
+                novo_carro.save()
+                return HttpResponse({'mensagem': 'Carro salvo no banco'})
+            else:
+                return render(request, 'carros/novo_carro.html', {'novo_carro_form': form_carro})
+    return redirect('login_user')
--- core/settings.py
+++ core/settings.py
@@ -136,3 +136,5 @@ MEDIA_ROOT = os.path.join('media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#nome da função
